5b866eaeb7f5ed9e4f158135ad3db7e2b9182df3
nikki dekerf <nikkidekerf@gmail.com>
2023-01-23 16:07:35 +0100

    filled in the validations and associations for 3 models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :horses
+  has_many :bookings
+  has_many :received_bookings, through: :horses, source: :bookings
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
 end
