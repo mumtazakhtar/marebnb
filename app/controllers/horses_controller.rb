ddf3ea3e2356e194083e094b117e3e08be577db5
mumtaz mohammed <mumtazmohammed08@gmail.com>
2023-01-23 17:33:42 +0100

    created horse controller and index page

---
+++ app/controllers/horses_controller.rb
@@ -0,0 +1,48 @@
+class HorsesController < ApplicationController
+  before_action :set_horse_id, only: %i[show edit update destroy]
+  def index
+    @horses = Horse.all
+  end
+
+  def show
+  end
+
+  def new
+    @horse = Horse.new
+  end
+
+  def create
+    @horse = Horse.new(horse_params)
+    if @horse.save
+      redirect_to horse_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if Horse.update(horse_params)
+      redirect_to horse_path, notice:'Updated Succesfully'
+    else
+      render :edit, status: unprocessable_entity
+    end
+  end
+
+  def destroy
+    @horse.destroy
+    redirect_to root, notice: 'Deleted succesfully'
+  end
+
+  private
+
+  def set_horse_id
+    @horse = Horse.find(params[:id])
+  end
+
+  def horse_params
+    params.require(:horse).permit(:name, :age, :description, :breed, :price, :location)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "horses#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :horses, except: [:index] do
+    resources :bookings
+  end
 end
