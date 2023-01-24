dc88cb060a6c10dfe90245520b32cdd93e6353ad
nikki dekerf <nikkidekerf@gmail.com>
2023-01-24 12:17:21 +0100

    The seed file now provides us with 10 horses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+# A horse has a name, age, breed, location, description, price and user_id
+# For starters, all horses are assigned to user 1 (is that needed?)
+
+# Check if it is good practice to destroy_all in the seeds file?
+puts 'First cleaning the database...'
+Horse.destroy_all
+puts 'Done!'
+
+puts 'Creating new horses'
+
+10.times do
+  Horse.create(
+    name: Faker::Name.name_with_middle,
+    age: rand(0..30),
+    breed: Faker::Creature::Horse.breed,
+    location: "Netherlands",
+    description: "What an amazing horse this is. Very horsey.",
+    price: rand(75...199),
+    user_id: 1
+  )
+end
+
+puts 'Done. Just created 10 horses.'
