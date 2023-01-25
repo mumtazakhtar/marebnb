8a09ee87b51ebfc5236645aa00a78f617e38c894
Sari Vos <vos.sari@gmail.com>
2023-01-25 12:28:36 +0100

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require 'faker'
+require "open-uri"
 
 # A horse has a name, age, breed, location, description, price and user_id
 # For starters, all horses are assigned to user 1 (is that needed?)
@@ -23,7 +24,7 @@ User.create(first_name: 'Nikki', last_name: 'kerpstra', email: 'alsomytruemail@m
 
 puts 'Creating new horses and assigning to users'
 10.times do
-  Horse.create(
+  horse = Horse.new(
     name: Faker::Name.name_with_middle,
     age: rand(0..30),
     breed: Faker::Creature::Horse.breed,
@@ -32,6 +33,9 @@ puts 'Creating new horses and assigning to users'
     price: rand(75...199),
     user: User.all.sample
   )
+  photo = URI.open("https://source.unsplash.com/random/?horse")
+  horse.photo.attach(io: photo, filename: "#{horse.name}.png", content_type: "image/png")
+  horse.save
 end
 
 puts 'Done. Just created 10 horses.'
