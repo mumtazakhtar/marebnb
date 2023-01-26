18eb2e071009e5351b5b72798eb442699a8a407e
nikki dekerf <nikkidekerf@gmail.com>
2023-01-26 10:29:23 +0100

    refactore

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :horse
   belongs_to :user
 
-  def initalize(days, price)
+  def initalize(days, price, symbol)
   end
 
   def days
@@ -13,5 +13,13 @@ class Booking < ApplicationRecord
   def price
     price = days * horse.price
     return price.to_i
+  end
+
+  def symbol
+    if status == "pending"
+      return "<i class='fa-solid fa-ban'></i>"
+    elsif status == "accepted"
+      return "<i class='fa-regular fa-square-check'></i>"
+    end
   end
 end
